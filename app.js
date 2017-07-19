174d1e429089d22331deb07edb915921f19a5409
dweb1 <david.p.weber@gmail.com>
2017-07-19 17:27:48 -0400

    completed GET routes

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+const express = require('express');
+const hbs = require('hbs');
 
-//require express package
-var express = require('express');
-//save an express module as 'app'
-var app     = express();
-// assigning 3000 as our port
-var port    = 3000;
+const app = express();
+app.set('view engine', 'hbs');
 
-// tells the server to listen for requests on port 3000
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+	res.send("Welcome to Pizza Express!")
+})
+
+app.get("/topping/:choice", (req, res) => {
+	let choice = req.params.choice;
+	res.send(`${choice} pizza! Good choice.`);
+})
+
+ app.get('/order/:amount/:size', (req, res) => {
+ 	let amount = req.params.amount;
+ 	let size = req.params.size;
+    res.send(`Your order for ${amount} ${size} pizzas will be ready in 1 minute!`);
+ });
+
+const port    = 3000;
 app.listen(port, function(){
   console.log("==========================")
   console.log('LISTENING ON PORT ' + port);
